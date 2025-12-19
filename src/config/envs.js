b7b57c73f0c18b242dbe1bdada007c306a0e2169
Angelsit0 <angelgabrieltp2006@gmail.com>
2025-12-18 23:55:58 -0400

    configuracion de redis

--- src/config/envs.js
+++ src/config/envs.js
@@ -1,5 +1,8 @@
-
 export const envs = {
     PORT: process.env.PORT || 3000,
     DATABASE_URL: process.env.DATABASE_URL,
+
+    // --- AGREGAMOS ESTO PARA REDIS ---
+    REDIS_HOST: process.env.REDIS_HOST || 'localhost',
+    REDIS_PORT: process.env.REDIS_PORT || 6379,
 };
